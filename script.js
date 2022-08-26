8e2ce5581b3bbfa7079f1e6a0f4555fe60f28353
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-08-26 19:56:52 +0200

    added createGrid function that creates selected number of Divs

---
+++ script.js
@@ -0,0 +1,14 @@
+function createGrid(size){
+    const grid = size * size;
+
+    for (let i=0; i<grid; i++){
+    
+    let gridGet=document.getElementById("gridContainer");
+    let newDiv = document.createElement('div');
+    gridGet.appendChild(newDiv);
+
+    }
+
+    }
+    
+
