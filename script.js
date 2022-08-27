b967cf02d01ed1c9ef12058d15144386213dd8d4
S-Guettner-Dev <sven_guettner@protonmail.com>
2022-08-27 21:42:56 +0200

    only one grid can be created

--- script.js
+++ script.js
@@ -2,15 +2,7 @@ function createGrid(size){
     /*Creating grid from gridSize Button input*/
     const grid = size * size;
 
-   
-   
-    /*Checking if div is empty */
-
     
-    
-    
-    
-   
     /*get the css var that defines the gridSize in gridbox*/
     let rootSelector = document.querySelector(':root');
     let rootChange = getComputedStyle(rootSelector);
@@ -39,17 +31,27 @@ switch(size){
         rootSelector.style.setProperty('--gridSize','repeat(128,50px');
     break;
 }
-    
+ 
 
-    /*Creating choosen number of divs*/
+/*Checking if div is empty */
+let gridGet=document.getElementById("gridContainer");
+let divs = document.getElementsByClassName("gridDivs");
+
+
+
+
+if (gridGet.childNodes.length <=1){
     for (let i=0; i<grid; i++){
     
-    let gridGet=document.getElementById("gridContainer");
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute ('class','gridDivs');
-    gridGet.appendChild(newDiv);
-    
+        let gridGet=document.getElementById("gridContainer");
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute ('class','gridDivs');
+        gridGet.appendChild(newDiv);
+        
+        }
     }
+    /*Creating choosen number of divs*/
+    
 
     
  
@@ -63,13 +65,22 @@ switch(size){
 
 
 
-    function removeGrid(){
-        
+    function removeGrid(size){
+        const grid = size * size;
         let checkGrid=document.getElementById("gridContainer");
-        
+        let divs = document.getElementsByClassName("gridDivs")
         console.log(checkGrid)
+    for (i=0;i<grid;i++){
+        divs[0].parentNode.removeChild(divs[0]);
+    }
         
-        if (checkGrid.childNodes.length != 0) {
-        checkGrid.parentNode.removeChild(checkGrid);
-        }
+        
+    }
+
+
+    function testDivContent(){
+        let divs = document.getElementsByClassName("gridDivs");
+        
+        let gridGet=document.getElementById("gridContainer");
+        console.log(gridGet.childNodes.length)
     }
